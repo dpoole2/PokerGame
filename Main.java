33f1ea530c6c8d2b3af99cc9af13c2f59fa600b3
gurkeleute <dylanb0708@gmail.com>
2017-02-24 10:55:18 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
+package Poker;
 /*
  * This is where the poker game will start
  * It will not be the actual game board
  */
-public class Main {
+public class Poker {
 	//These arrays will now just be unidealized
 	static char [][] fullDeck = new char[3][12];
 	static char[] cards= new char[12];
@@ -12,7 +13,9 @@ public class Main {
 public static void main(String args[] ){
 	//pass the arrays to the DeckCreator class
 	DeckCreator deckGen = new DeckCreator(cards, fullDeck);
-	System.out.println("ALl GOOD");
+        //After this the deck will be created 
+        //Have a translater class ready 
+        //so that the user can read what card they got
 	deckGen.cardGen();
 }
 }
