8e22261cac193cb34eeb1604a5c2e1ba097a339b
gurkeleute <dylanb0708@gmail.com>
2017-02-24 10:54:07 -0500

    Create CardTranslate.java

---
+++ CardTranslate.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poker;
+
+/**
+ *
+ * @author Dylan
+ */
+public class CardTranslate {
+    private static char[][] deck = new char[4][52];
+    public CardTranslate(char fullDeck[][]){
+        fullDeck= deck;
+    }
+}
